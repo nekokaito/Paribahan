32427231beaea187e9d2343a82ac4247b5f854cc
nekokaito <siddiqsazzad001@gmail.com>
2024-02-18 22:44:46 +0600

    added some color functions

--- scripts/click.js
+++ scripts/click.js
@@ -1,20 +1,28 @@
 function Click(Id, call) {
     document.getElementById(Id).addEventListener('click', call);
   }
+  function addColor (Id) {
+    const me = document.getElementById(Id);
+    me.classList.add('bg-green-500');
+  }
 
 Click('a1', function() {
+    addColor('a1');
     btnCount(); 
     calcValue(); 
 });
 Click('a2', function() {
+    addColor('a2');
     btnCount(); 
     calcValue(); 
 });
 Click('a3', function() {
+    addColor('a3');
     btnCount(); 
     calcValue(); 
 });
 Click('a4', function() {
+    addColor('a4');
     btnCount(); 
     calcValue(); 
 });
